7df7f55107f2942a42db55de93f7af0ec1e7174f
Mdr-Overflow <madarasandreii@gmail.com>
2023-05-17 01:10:06 +0300

    MERERERERERERER

--- newChunking2.c
+++ newChunking2.c
@@ -1,15 +1,9 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <math.h>
 
-#define MAX_CHUNK_SIZE 8000// Define your desired chunk size here
-#define NUM_THREADS 8   // Define the number of threads to use
-
-int rem_size = 0;
+#define CHUNK_SIZE 8000 // Define your desired chunk size here
+#define NUM_THREADS 4   // Define the number of threads to use
 
 struct ThreadData {
   FILE *file;
@@ -37,63 +31,25 @@ void *splitImage(void *arg) {
   struct ThreadData *threadData = (struct ThreadData *)arg;
   FILE *file = threadData->file;
   int chunkIndex = threadData->chunkIndex;
-
-  // int actual_chunk_size = threadData ->actualSize;
   unsigned char *buffer = threadData->buffer;
 
-  if (rem_size >= MAX_CHUNK_SIZE){
+  fseek(file, chunkIndex * CHUNK_SIZE, SEEK_SET);
 
-  fseek(file, chunkIndex * MAX_CHUNK_SIZE, SEEK_SET);
-
-  size_t bytesRead = fread(buffer, 1, MAX_CHUNK_SIZE, file);
-  if (bytesRead < MAX_CHUNK_SIZE) {
+  size_t bytesRead = fread(buffer, 1, CHUNK_SIZE, file);
+  if (bytesRead < CHUNK_SIZE) {
     printf("Error reading image chunk\n");
     pthread_exit(NULL);
   }
-  
 
   printf("Chunk %d read\n", chunkIndex);
 
-  fseek(file, (long)bytesRead,
+  fseek(file, -(long)bytesRead,
         SEEK_CUR); // Set file position back to the original position
-  }
 
-  else {
-  
-  size_t bytesRead = fread(buffer, 1, rem_size, file);
-
-
-
-  }
-
-  rem_size -= MAX_CHUNK_SIZE;
   pthread_exit(NULL);
 }
 
 int main(int argc, char **argv) {
-
-   if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        exit(1);
-    }
-
-     char *filename = argv[1];
-
-    struct stat st;
-    if (stat(filename, &st) != 0) {
-        perror("stat");
-        exit(1);
-    }
-
-    if (!S_ISREG(st.st_mode)) {
-        fprintf(stderr, "%s is not a regular file\n", filename);
-        exit(1);
-    }
-
-    printf("%s: %lu bytes\n", filename, st.st_size);
-
-
-
   FILE *file =
       fopen(argv[1], "rb"); // Change "input.jpg" to your image file name
   if (file == NULL) {
@@ -105,9 +61,9 @@ int main(int argc, char **argv) {
   long fileSize = ftell(file);
   fseek(file, 0, SEEK_SET);
 
-  int numChunks = (fileSize + MAX_CHUNK_SIZE - 1) / MAX_CHUNK_SIZE;
+  int numChunks = (fileSize + CHUNK_SIZE - 1) / CHUNK_SIZE;
 
-  unsigned char *buffer = (unsigned char *)malloc(numChunks * MAX_CHUNK_SIZE);
+  unsigned char *buffer = (unsigned char *)malloc(numChunks * CHUNK_SIZE);
   if (buffer == NULL) {
     printf("Failed to allocate memory for chunks\n");
     fclose(file);
@@ -122,13 +78,8 @@ int main(int argc, char **argv) {
 
   int currentChunkIndex = 0;
 
-  rem_size = fileSize;
-
   for (int i = 0; i < NUM_THREADS; i++) {
     int chunkCount = threadChunkCount;
-
-    if ( rem_size == 0) break;
-
     if (remainingChunks > 0) {
       chunkCount++;
       remainingChunks--;
@@ -136,20 +87,8 @@ int main(int argc, char **argv) {
 
     threadData[i].file = file;
     threadData[i].chunkIndex = currentChunkIndex;
+    threadData[i].buffer = buffer + (currentChunkIndex * CHUNK_SIZE);
 
-    if (rem_size > MAX_CHUNK_SIZE){
-
-        threadData[i].buffer = buffer + (currentChunkIndex * MAX_CHUNK_SIZE);
-    }
-
-    else {
-    threadData[i].buffer = buffer + (currentChunkIndex * rem_size);
-      
-    }
-
-    
-   
-    rem_size -= MAX_CHUNK_SIZE;
     currentChunkIndex += chunkCount;
 
     if (pthread_create(&threads[i], NULL, splitImage, (void *)&threadData[i]) !=
@@ -170,27 +109,29 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Print the content of the buffer
+  int lastChunkSize = fileSize % CHUNK_SIZE;
+if (lastChunkSize > 0) {
+fseek(file, -(long)lastChunkSize, SEEK_END);
+size_t bytesRead = fread(buffer + (numChunks - 1) * CHUNK_SIZE, 1, lastChunkSize, file);
+if (bytesRead < lastChunkSize) {
+printf("Error reading last image chunk\n");
+free(buffer);
+fclose(file);
+return 1;
+}
+printf("Last chunk read\n");
+}
+
+//   Print the content of the buffer
   printf("Buffer content:\n");
   for (int i = 0; i < numChunks; i++) {
     printf("Chunk %d:\n", i);
-    for (int j = 0; j < MAX_CHUNK_SIZE; j++) {
-      printf("%02X ", buffer[i * MAX_CHUNK_SIZE + j]);
+    for (int j = 0; j < CHUNK_SIZE; j++) {
+      printf("%02X ", buffer[i * CHUNK_SIZE + j]);
     }
     printf("\n\n");
   }
-    // while (fileSize != 0) {
-
-    // if (fileSize >= MAX_CHUNK_SIZE){  
-    writeChunkToFile("output11.png", buffer, fileSize);
-    // fileSize -= MAX_CHUNK_SIZE;
-    // }
-    // else {
-    // writeChunkToFile("output11.png", buffer, fileSize);
-    // }
-
-    // }
-    
+    writeChunkToFile("output11.png", buffer, fileSize );
   free(buffer);
   fclose(file);
   return 0;
